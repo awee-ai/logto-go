70c440da37a7fae8dd4d442d1b6a000fd91d73ec
Ignas Bernotas <ignas@iber.lt>
2025-09-20 16:40:32 +0300

    chore: logging

--- client/handle_sign_in_callback.go
+++ client/handle_sign_in_callback.go
@@ -5,28 +5,38 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/toaweme/log"
+
 	"github.com/logto-io/go/v2/core"
 )
 
 func (logtoClient *LogtoClient) HandleSignInCallback(request *http.Request) error {
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "starting")
 	signInSession := SignInSession{}
 	parseSignInSessionErr := json.Unmarshal([]byte(logtoClient.storage.GetItem(StorageKeySignInSession)), &signInSession)
 
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "parsed-sign-in-session", "error", parseSignInSessionErr)
 	if parseSignInSessionErr != nil {
 		return parseSignInSessionErr
 	}
 
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "retrieving-code")
 	callbackUri := GetOriginRequestUrl(request)
 	code, retrieveCodeErr := core.VerifyAndParseCodeFromCallbackUri(callbackUri, signInSession.RedirectUri, signInSession.State)
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "retrieved-code", "error", retrieveCodeErr)
 	if retrieveCodeErr != nil {
 		return retrieveCodeErr
 	}
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "fetching-oidc-config")
 
 	oidcConfig, fetchOidcConfigErr := logtoClient.fetchOidcConfig()
 
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "fetched-oidc-config", "error", fetchOidcConfigErr)
 	if fetchOidcConfigErr != nil {
 		return fetchOidcConfigErr
 	}
+
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "fetching-token")
 
 	codeTokenResponse, fetchTokenErr := core.FetchTokenByAuthorizationCode(logtoClient.httpClient, &core.FetchTokenByAuthorizationCodeOptions{
 		TokenEndpoint: oidcConfig.TokenEndpoint,
@@ -37,9 +47,12 @@ func (logtoClient *LogtoClient) HandleSignInCallback(request *http.Request) erro
 		RedirectUri:   signInSession.RedirectUri,
 	})
 
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "fetched-token", "error", fetchTokenErr)
 	if fetchTokenErr != nil {
 		return fetchTokenErr
 	}
+
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "clearing-sign-in-session")
 
 	logtoClient.storage.SetItem(StorageKeySignInSession, "")
 
@@ -48,6 +61,8 @@ func (logtoClient *LogtoClient) HandleSignInCallback(request *http.Request) erro
 		Scope:     codeTokenResponse.Scope,
 		ExpiresAt: time.Now().Unix() + int64(codeTokenResponse.ExpireIn),
 	}
+
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "verifying-and-saving-token-response")
 
 	// - Treat `scopes` as `empty` to construct the default access token key
 	accessTokenKey := buildAccessTokenKey([]string{}, "", "")
@@ -58,6 +73,7 @@ func (logtoClient *LogtoClient) HandleSignInCallback(request *http.Request) erro
 		accessToken,
 		&oidcConfig,
 	)
+	log.Debug("logto", "call", "handle-sign-in-callback", "status", "verified-and-saved-token-response", "error", verificationErr)
 
 	if verificationErr != nil {
 		return verificationErr
